60c0de1cd92b1061ff2f551549a4787f65cb6d8a
DuckBroApprentice <pigbirthin0320@gmail.com>
2025-04-17 20:04:06 +0800

    insertionSort實現

---
+++ ClassOne/insertionSort.go
@@ -0,0 +1,12 @@
+package classone
+
+func insertionSort(nums []int) {
+	for i := 1; i < len(nums); i++ {
+		for j, k := i-1, i; j >= 0; j, k = j-1, k-1 {
+			if nums[k] > nums[j] {
+				break
+			}
+			nums[j], nums[k] = nums[k], nums[j]
+		}
+	}
+}
