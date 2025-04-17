f07905d9a359cc0d913ebb7cb243db713df65066
DuckBroApprentice <pigbirthin0320@gmail.com>
2025-04-17 20:44:18 +0800

    暫時找靈感

--- ClassOne/mergeSort.go
+++ ClassOne/mergeSort.go
@@ -39,13 +39,41 @@ func mergeSort(nums []int) {
 			這個改動要能影響到原數據(nums)
 			linked-list?
 			{
+			type ListNode struct {
+				Val int
+				Next *ListNode
+			}
 			要先歷遍長度 for head != nil {lenght++}
+			if length <= 2 {
+				return
+			}
 			for i:=0;i<lnght /2 ;i++{left}
 			for i:=length /2;i<length;i++{right}
+			func(left)
+
+			func(right)
 			}
+			這樣處理每一次都會有一個O(n)遍歷長度
+			再來切割*2  +起來也是一個O(n)
+			一直到length <= 2時，分的層數會是O(logn)的時間複雜度
 			[]*int可行嗎?
 			{
 			似乎很麻煩  要給每個地址都賦值 [2fac49] *[2fac49] = [1]
+			好像想錯了
+			已知array/slice 是一個連續的記憶體空間
+			原始資料[]int
+			每一個元素都有自己的記憶體位址
+			想法不應該是[]*int  這個獨立的陣列操作並不會影響到原始陣列
+			而是指向這個陣列中的元素修改值
+			結論又回到鏈表了 XD
+
+			另外有個想法  //我大概率是處理不了
+			int64的記憶體分配是 size:8 align:8 (內存對齊)
+			nums []int
+			numspoint := &nums
+			就能獲得nums的記憶體位址
+			透過規則可以推算每個元素的記憶體位址
+			不過其實也不知道能怎麼操作  想想就好
 			}
 	}
 	right{
