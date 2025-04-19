12ae87d46ddc8ebfbfd01b2d3160cbce73f0cac1
DuckBroApprentice <pigbirthin0320@gmail.com>
2025-04-19 09:29:07 +0800

    stack complete, unit-test by leetcode

---
+++ ClassOne/stack.go
@@ -0,0 +1,36 @@
+// leetcode Q20:Valid Parentheses
+package classone
+
+func isValid(s string) bool {
+	var stack []string
+	for i := 0; i < len(s); i++ {
+		if string(s[i]) == "(" || string(s[i]) == "[" || string(s[i]) == "{" {
+			stack = append(stack, string(s[i]))
+		}
+		if (string(s[i]) == ")" || string(s[i]) == "]" || string(s[i]) == "}") && len(stack) == 0 {
+			return false
+		}
+		if string(s[i]) == ")" {
+			if stack[len(stack)-1] != "(" {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+		if string(s[i]) == "]" {
+			if stack[len(stack)-1] != "[" {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+		if string(s[i]) == "}" {
+			if stack[len(stack)-1] != "{" {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+	return len(stack) == 0
+}
+
+//Runtime 0ms Beats 100%
+//Memory 4.72MB Beats 9.04%
