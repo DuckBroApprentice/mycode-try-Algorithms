5b70584b0c97cb7eaaf8c0fd13426408d7bcd7fb
DuckBroApprentice <pigbirthin0320@gmail.com>
2025-04-20 18:03:50 +0800

    inorderTraversal

--- ClassThree/DFS.go
+++ ClassThree/DFS.go
@@ -64,3 +64,28 @@ func dfs(grid [][]byte, i, j int) [][]byte {
 //Runtime 4ms Beats 70.88%
 //Memory 5.64MB Beats 89.28%
 //unit test by leetcode
+
+//leetcode Q94. Binary Tree Inorder Traversal
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func inorderTraversal(root *TreeNode) []int {
+	var res []int
+	var inorder func(node *TreeNode)
+	inorder = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		inorder(node.Left)
+		res = append(res, node.Val)
+		inorder(node.Right)
+	}
+	inorder(root)
+	return res
+}
+
+//以前好像有解過inorder
