9d2eefc17c38fa879bdd4e12df3dcd9ff7a58400
DuckBroApprentice <pigbirthin0320@gmail.com>
2025-04-20 15:06:04 +0800

    隨便練習一個遞迴

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
-import (
-	classtwo "Algorithms/ClassTwo"
-	"fmt"
-)
+import "fmt"
+
+// func main() {
+// 	for i := 1; i <= 100; i++ {
+// 		fmt.Printf("input i: %d,alive i: %d\n", i, classtwo.Josephus(i))
+// 	}
+// }
+
+func nStep(n int) int {
+	if n == 1 {
+		return 1
+	}
+	return n * nStep(n-1)
+}
 
 func main() {
-	for i := 1; i <= 100; i++ {
-		fmt.Printf("input i: %d,alive i: %d\n", i, classtwo.Josephus(i))
-	}
+	a := nStep(9)
+	fmt.Print(a)
 }
