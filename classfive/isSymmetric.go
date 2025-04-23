51ba55f94c577dde08c18f7f9b73d9b6f4d9ad02
DuckBroApprentice <pigbirthin0320@gmail.com>
2025-04-23 15:12:15 +0800

    補上思路

--- classfive/isSymmetric.go
+++ classfive/isSymmetric.go
@@ -8,49 +8,37 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-// func isSymmetric(root *TreeNode) bool {
-// 	var stack []int
-// 	preAndPostOrder(root, stack)
-// 	for i, j := 0, len(stack)-1; i < j; i, j = i+1, j-1 {
-// 		if stack[i] != stack[j] {
-// 			return false
-// 		}
-// 	}
-// 	return true
-// }
+/*
+思路：
+用一個陣列存左右半邊的pre+post order
+陣列前->後：左半邊的pre  (會混到post)
+陣列後->前：右半邊的post (同樣會混到pre)
+雖然都有混到不需要的數據，但是 左 post 同樣也會是 右 pre  (陣列夾著看的話)
+核心在比較左的pre跟右的post，且為了使陣列能真實反映樹，nil都補上101(-100<=val<=100)
+*/
 
-// func preAndPostOrder(root *TreeNode, stack []int) []int {
-// 	if root == nil {
-// 		stack = append(stack, 101)
-// 		return stack
-// 	}
-// 	stack = append(stack, root.Val)
-// 	stack = preAndPostOrder(root.Left, stack)
-// 	stack = preAndPostOrder(root.Right, stack)
-// 	stack = append(stack, root.Val)
-// 	return stack
-// }
+func isSymmetric(root *TreeNode) bool {
+	var stack []int
+	preAndPostOrder(root, stack)
+	for i, j := 0, len(stack)-1; i < j; i, j = i+1, j-1 {
+		if stack[i] != stack[j] {
+			return false
+		}
+	}
+	return true
+}
+
+func preAndPostOrder(root *TreeNode, stack []int) []int {
+	if root == nil {
+		stack = append(stack, 101)
+		return stack
+	}
+	stack = append(stack, root.Val)
+	stack = preAndPostOrder(root.Left, stack)
+	stack = preAndPostOrder(root.Right, stack)
+	stack = append(stack, root.Val)
+	return stack
+}
+
 // Runtime 89ms Beats 2.39%
 // Memory 8.84MB Beats 4.38%
-func isSymmetric(root *TreeNode) bool {
-	if root.Left.Val != root.Right.Val {
-		return false
-	}
-	return startLeft(root.Left) == startRight(root.Right)
-}
-func startLeft(root *TreeNode) int {
-	if root == nil {
-		return 101
-	}
-	startLeft(root.Left)
-	startLeft(root.Right)
-	return root.Val
-}
-func startRight(root *TreeNode) int {
-	if root == nil {
-		return 101
-	}
-	startRight(root.Right)
-	startRight(root.Left)
-	return root.Val
-}
